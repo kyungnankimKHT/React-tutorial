ca0f48000720582ca3092a90c64658af1bee70ad
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-07-03 12:49:34 +0900

    props

--- tutorial-one/src/App.js
+++ tutorial-one/src/App.js
@@ -19,6 +19,7 @@ import R01 from "./R01_ClassComponent.js";
 import R02 from "./R02_FunctionComponent.js";
 import Props1 from "./R03_Prop.js";
 import Props2 from "./R04_Props2.js";
+import Props3 from "./R05_Props3.js";
 function App() {
   return (
     /* 
@@ -29,21 +30,26 @@ function App() {
     <div className="App-header">
       {/*<R01/>*/}
       {/*<R02/>*/}
+      {/*
       <Props1 num="1" name="홍길동" age="20" score="90" />
       <Props1 num="2" name="홍길은" age="30" score="80" />
       <Props1 num="3" name="홍길금" age="40" score="70" />
       <h3>Props 예제 2번 사용하기</h3>
-      <Props2 name="강감찬" age="72" gender="남자" />
+      <Props2 name="강감찬" age="72" gender="남자" />*
+      /}
       {/*  
-
       <Props2 name="강감찬" age="72" gender="남자" />
-
       Props2 객체는 아래와 같은 코드를 한 줄로 작성하기 위해 가져온 이름
-
       <div className="info">
         이름 : 강감찬 / 나이 : 72 / 성별 : 남자
       </div>
       */}
+      <Props3 num='5' name="김연정" age='40' gender='여자' phone='010-1111-2222'/>
+
+
+
+
+
     </div>
   );
 }
