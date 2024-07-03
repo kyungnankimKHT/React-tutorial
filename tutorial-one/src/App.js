f8a02e0978399905f87e81ad39040f6c8306f492
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-07-03 12:32:16 +0900

    props

--- tutorial-one/src/App.js
+++ tutorial-one/src/App.js
@@ -1,5 +1,4 @@
-
-import './App.css';/* CSS를 얻어와 아래 작성된 태그 옆에 className으로 적용 */
+import "./App.css"; /* CSS를 얻어와 아래 작성된 태그 옆에 className으로 적용 */
 /* 
 기본 html 에서는 css를 적용할 때 class 라는 이름을 사용하지만
 React에서 작성한 html에는 css를 적용할 때 className 이라는 이름을 사용
@@ -16,15 +15,35 @@ R01_ClassComponent.js를 가져와서 App.js에 적용
 R01_ClassComponent.js R01 이라는 별칭을 사용해서 App.js 적용
 */
 
-import R01 from './R01_ClassComponent';
-import R02 from './R02_FunctionComponent';
+import R01 from "./R01_ClassComponent.js";
+import R02 from "./R02_FunctionComponent.js";
+import Props1 from "./R03_Prop.js";
+import Props2 from "./R04_Props2.js";
 function App() {
-
   return (
+    /* 
+    리액트는 알아서 새로고침을 주기적으로 자주 진행
+    주석 :   / * * /   -> 설명이나 추가 내용을 작성할 때 사용
+          { / * * /}   -> 주로 코드를 주석 처리할 때 사용
+    */
     <div className="App-header">
-     <R01/>
-     <p>--------------------</p>
-     <R02/>
+      {/*<R01/>*/}
+      {/*<R02/>*/}
+      <Props1 num="1" name="홍길동" age="20" score="90" />
+      <Props1 num="2" name="홍길은" age="30" score="80" />
+      <Props1 num="3" name="홍길금" age="40" score="70" />
+      <h3>Props 예제 2번 사용하기</h3>
+      <Props2 name="강감찬" age="72" gender="남자" />
+      {/*  
+
+      <Props2 name="강감찬" age="72" gender="남자" />
+
+      Props2 객체는 아래와 같은 코드를 한 줄로 작성하기 위해 가져온 이름
+
+      <div className="info">
+        이름 : 강감찬 / 나이 : 72 / 성별 : 남자
+      </div>
+      */}
     </div>
   );
 }
