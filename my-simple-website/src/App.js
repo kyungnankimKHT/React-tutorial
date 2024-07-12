c5b90d14573e709206eb906dfcc214f2c7c167d1
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-07-12 11:13:04 +0900

    movie

--- my-simple-website/src/App.js
+++ my-simple-website/src/App.js
@@ -10,19 +10,21 @@ import TodoList from "./component/TodoList";
 import TicTapToe from "./component/TicTapToe";
 import TicTapToeTwoStep from "./component/TicTapToeTwoStep";
 import TypingTest from "./component/TypingTest";
+import MovieRating from "./component/Movie/MovieGrade";
 function App() {
   return (
     <div>
       <Header />
       <NavBar />
       <Routes> {/*링크 모음  예전에는 Switch 라고 작성했지만 v6부터 Routes 이름 사용 */}
-        <Route path="/"             element =  {   <Home />   } />
-        <Route path="/game"         element =  {   <Game />   } />
-        <Route path="/game-twoStep" element =  {   <GameTwoStep />   } />
-        <Route path="/todoList"     element =  {   <TodoList />} />
-        <Route path="/tictaptoe"    element =  {   <TicTapToe/>} />
+        <Route path="/"             element =  {   <Home        />    } />
+        <Route path="/game"         element =  {   <Game        />    } />
+        <Route path="/game-twoStep" element =  {   <GameTwoStep />    } />
+        <Route path="/todoList"     element =  {   <TodoList    />    } />
+        <Route path="/tictaptoe"    element =  {   <TicTapToe   />    } />
         <Route path="/ttt-twoStep"  element =  {   <TicTapToeTwoStep/>} />
-        <Route path="/typingTest"  element =  {   <TypingTest/>} />
+        <Route path="/typingTest"   element =  {   <TypingTest  />    } />
+        <Route path="/movieRate"    element =  {   <MovieRating />    } />
       </Routes>
 
 
--- my-simple-website/src/component/Movie/MovieGrade.js
+++ my-simple-website/src/component/Movie/MovieGrade.js
@@ -7,9 +7,19 @@ const MovieRating = () => {
   const [영화들, set영화들] = useState([]);
 
   useEffect(() => {
+    axios.get('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year')
+    .then(res => {
+        // 위 url은 [] 가 아니라 {} 에서 시작하므로 {}데이터를 감싸줄
+        // 임의의 변수명 res사용 res = [] .data 데이터를 가져오겠다
+        // 주소값에 key 이름으로 작성된 data 에서 movie라는 키 값 안에 적힌 title과 이미지들을 가져올 것
+        //         [   {  data{ movie{ 를 가져올 것
+        set영화들(res.data.data.movies);//{}중괄호 들에서 data를 가지고오겠다.
+    })
+    .catch(err => {
+        alert(err + " 발생했습니다.");
+    });
 
 
-    
   }, []);
 
 
