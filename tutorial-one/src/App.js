8852b00040daf7e329a2d14e731c6cda4715d1ec
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-07-12 17:26:23 +0900

    img profile

--- tutorial-one/src/App.js
+++ tutorial-one/src/App.js
@@ -47,6 +47,7 @@ import Fetch예제2번 from './component/R29_Fetch_Ex2.js';
 
 import Board from "./component/R30_PagiNation/Board.js";
 import Album from "./component/R31_PagiNationEx/Album.js";
+import Profile from './component/R32_Profile.js';
 function App() {
   return (
     /* 
@@ -95,9 +96,11 @@ function App() {
       <Axios예제1번 />
       <Fetch예제1번 />
       <Fetch예제2번 />
+      <Album />
        */}
       
-      <Album />
+      
+      <Profile />
     </div>
   );
 }
